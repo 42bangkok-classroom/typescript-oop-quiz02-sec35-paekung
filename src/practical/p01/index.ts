111380325e9949070503d46970a927f15c43bb0a
paekung <supakit.3t@gmail.com>
2026-01-16 09:33:57 +0700

    p01

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -1,1 +1,38 @@
-export function getEdgePosts() {}
+import axios from 'axios';
+
+interface Post {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+}
+
+interface EdgePost {
+  id: number;
+  title: string;
+}
+
+export async function getEdgePosts(){
+  try {
+    const response = await axios.get<Post[]>('https://jsonplaceholder.typicode.com/posts');
+    const posts = response.data;
+
+    if (posts.length === 0) {
+      return [];
+    }
+
+    const firstPost: EdgePost = {
+      id: posts[0].id,
+      title: posts[0].title,
+    };
+
+    const lastPost: EdgePost = {
+      id: posts[posts.length - 1].id,
+      title: posts[posts.length - 1].title,
+    };
+
+    return [firstPost, lastPost];
+  } catch (error) {
+    return [];
+  }
+}
