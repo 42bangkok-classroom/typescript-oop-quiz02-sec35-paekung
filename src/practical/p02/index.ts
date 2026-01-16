11568ef862742a5979b47568584af6cd847105ad
paekung <supakit.3t@gmail.com>
2026-01-16 09:48:26 +0700

    p02

--- src/practical/p02/index.ts
+++ src/practical/p02/index.ts
@@ -1,1 +1,29 @@
-export function getPostsByUser() {}
+import axios from "axios";
+
+interface Post {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+}
+
+export async function getPostsByUser(userId: number) {
+  try {
+    const response = await axios.get<Post[]>('https://jsonplaceholder.typicode.com/posts');
+    const posts = response.data;
+
+    const userPosts = posts.filter(post => post.userId === userId);
+    
+    const result = userPosts.map((post) => ({
+      id: post.id,
+      userId: post.userId,
+      title: post.title,
+      body: post.body,
+    }));
+
+    return result;
+  }
+  catch (error) {
+    return [];
+  }
+}
