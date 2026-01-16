ba753b993a1de7f809bd94f2ae8f6ca73d39f4ed
paekung <supakit.3t@gmail.com>
2026-01-16 10:51:10 +0700

    p05

--- src/practical/p05/index.ts
+++ src/practical/p05/index.ts
@@ -1,2 +1,31 @@
-export function safeFetchComment() {}
+import axios from "axios";
 
+interface Comments {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+}
+
+interface ResultComment {
+  id: number;
+  body: string;
+}
+
+export async function safeFetchComment(id: number) {
+  try {
+    const responseComments = await axios.get<Comments>(`https://jsonplaceholder.typicode.com/comments/${id}`);
+    const comment = responseComments.data;
+
+    const result: ResultComment = {
+      id: comment.id,
+      body: comment.body,
+    };
+
+    return result;
+  }
+  catch {
+    return null;
+  }
+}
