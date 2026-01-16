4c169215b35a0d6659d89b6aec17135bf7984b78
paekung <supakit.3t@gmail.com>
2026-01-16 10:51:51 +0700

    p04

--- src/practical/p04/index.ts
+++ src/practical/p04/index.ts
@@ -10,6 +10,26 @@ interface Comments {
 
 export async function countCommentsByPost() {
   try {
+    const responseComments = await axios.get<Comments[]>('https://jsonplaceholder.typicode.com/comments');
+    const comments = responseComments.data;
+
+    // {
+    //   1: 5,
+    //   2: 5,
+    //   3: 5
+    // }
+
+    const commentCountByPost = {};
+    
+    comments.forEach(comment => {
+      if (commentCountByPost[comment.postId]) {
+        commentCountByPost[comment.postId] += 1;
+      } else {
+        commentCountByPost[comment.postId] = 1;
+      }
+    });
+
+    return commentCountByPost;
     
   } catch (error) {
     return [];
